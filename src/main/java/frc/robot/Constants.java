77827ef96525a857892273e75b20c6e116d56ec1
Ryan Fitz-Gerald <ztif33@gmail.com>
2025-09-27 14:57:18 -0400

    Fixed motor IDs and gear ratio

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,11 +7,11 @@ import edu.wpi.first.units.measure.Distance;
 public class Constants {
     // Drivetrain Constants
     public static final Distance wheelDiameter = Inches.of(3);
-    public static final double driveRatio = 1.0/8.45;
+    public static final double driveRatio = 1.0/7.31;
 
     // CAN IDs
     public static final int lfDriveMotorID = 1;
-    public static final int lrDriveMotorID = 2;
-    public static final int rfDriveMotorID = 3;
+    public static final int lrDriveMotorID = 3;
+    public static final int rfDriveMotorID = 2;
     public static final int rrDriveMotorID = 4;
 }
